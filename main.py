c2b43c52946465326b65c168d4068151d1bb9bf6
Valeriia Nazarenko <valeria.naz1508@gmail.com>
2026-02-04 21:49:30 +0200

    main

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
+from src.game import Game
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+def main():
+    minesweeper = Game()
+    minesweeper.run()
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('hello Lera!!!')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    main()
