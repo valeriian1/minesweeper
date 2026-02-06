e83fe2c7f86d8ce4e29e3df3e0fd1836bea89d91
Денисюк Катерина <katjadenusjuk123@gmail.com>
2026-02-07 00:26:43 +0200

    feat: implement end screen
    Added end screen window
    Implemented interactive restart button with hover logic

--- src/ui/renderer.py
+++ src/ui/renderer.py
@@ -15,9 +15,15 @@ class GameRenderer:
         self.open_mask.set_alpha(80)
         self.open_mask.fill((0, 0, 50))
 
+        # Створюємо напівпрозору чорну оверлей поверх всього екрану (для віконця кінець гри)
+        self.overlay = pygame.Surface((1000, 1000)) # З запасом
+        self.overlay.set_alpha(180)
+        self.overlay.fill((0, 0, 0))
+
         # Завантажуємо спрайти
         board_sprite_path = "assets/sprites/board sprites"
         header_sprite_path = "assets/sprites/header sprites"
+        endscreen_sprite_path = "assets/sprites/endscreen sprites"
 
         raw_sprites = {
             'tile1': "gridTile1.png", 'tile2': "gridTile2.png",
@@ -63,6 +69,26 @@ class GameRenderer:
         self.board_line = [
             pygame.image.load(os.path.join(header_sprite_path, "boardLine1.png")).convert_alpha(),
             pygame.image.load(os.path.join(header_sprite_path, "boardLine2.png")).convert_alpha()
+        ]
+
+        self.endscreen_frames = [
+            pygame.image.load(os.path.join(endscreen_sprite_path, "window1.png")).convert_alpha(),
+            pygame.image.load(os.path.join(endscreen_sprite_path, "window2.png")).convert_alpha()
+        ]
+
+        self.trophy_frames = [
+            pygame.image.load(os.path.join(endscreen_sprite_path, "trophy1.png")).convert_alpha(),
+            pygame.image.load(os.path.join(endscreen_sprite_path, "trophy2.png")).convert_alpha()
+        ]
+        
+        self.skull_frames = [
+            pygame.image.load(os.path.join(endscreen_sprite_path, "skull1.png")).convert_alpha(),
+            pygame.image.load(os.path.join(endscreen_sprite_path, "skull2.png")).convert_alpha()
+        ]
+
+        self.restart_btn_frames = [
+            pygame.image.load(os.path.join(endscreen_sprite_path, "restart_btn1.png")).convert_alpha(),
+            pygame.image.load(os.path.join(endscreen_sprite_path, "restart_btn2.png")).convert_alpha()
         ]
 
     def get_cell_from_pos(self, pos):
@@ -137,3 +163,60 @@ class GameRenderer:
         s_value = str(val).zfill(3)
         for i, digit in enumerate(s_value):
             self.screen.blit(self.header_digits[int(digit)], (x + i * 19, y))
+
+    def draw_end_screen(self, status, current_time, best_time):
+        """
+        Відображає вікно кінця гри (перемога або поразка).
+        """
+        sw = self.screen.get_width()
+        sh = self.screen.get_height()
+        
+        # Малюємо затемнення
+        self.screen.blit(self.overlay, (0, 0))
+        
+        # Визначаємо індекс кадру (кожні 500 мс)
+        frame_idx = (pygame.time.get_ticks() // 500) % 2
+        
+        # Малюємо базове вікно
+        win_img = self.endscreen_frames[frame_idx]
+        win_x = (sw - win_img.get_width()) // 2
+        win_y = (sh - win_img.get_height()) // 2
+        self.screen.blit(win_img, (win_x, win_y))
+        
+        # Малюємо іконки для поточного та найкращого часу
+        self.screen.blit(self.trophy_frames[frame_idx], (win_x, win_y))
+        self.screen.blit(self.icon_clock[frame_idx], (win_x + 40, win_y + 65))
+        
+        # Малюємо череп (при програші)
+        if status == 'lose':
+            self.screen.blit(self.skull_frames[frame_idx], (win_x, win_y))
+
+        #в майбутньому додати спрайт для перемоги...
+        
+        # Малюємо цифри
+        # Поточний результат
+        self.draw_header_number(current_time, win_x + 20, win_y + 110)
+        
+        # Найкращий результат сесії
+        self.draw_header_number(best_time, win_x + 180, win_y + 110)
+
+        # кнопка RESTART
+        mouse_pos = pygame.mouse.get_pos()
+        # Розраховуємо позицію кнопки
+        self.btn_rect = pygame.Rect(win_x+110, win_y+110, 75, 50)
+        
+        # Перевірка: чи наведена миша на кнопку
+        if self.btn_rect.collidepoint(mouse_pos):
+            btn_img = self.restart_btn_frames[1]
+            draw_x = self.btn_rect.x-10
+            draw_y = self.btn_rect.y
+        else:
+            btn_img = self.restart_btn_frames[0]
+            draw_x = self.btn_rect.x-2
+            draw_y = self.btn_rect.y
+            
+        self.screen.blit(btn_img, (draw_x, draw_y))
+
+    def is_restart_clicked(self, pos):
+        """Викликається в main.py. Перевіряє, чи клік був по кнопці рестарту."""
+        return hasattr(self, 'btn_rect') and self.btn_rect.collidepoint(pos)
